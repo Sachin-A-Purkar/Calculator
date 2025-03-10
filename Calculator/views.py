932458d7dde7df85197705090fcd02cccfb7eb64
168742464+Sachin-A-Purkar@users.noreply.github.com <168742464+Sachin-A-Purkar@users.noreply.github.com>
2025-03-10 15:31:09 +0530

    update

--- Calculator/views.py
+++ Calculator/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Calculator
 
-# Create your views here.
+class CalculatorListView(ListView):
+    model = Calculator
+    template_name = "Calculator/index.html"
