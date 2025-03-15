3ba613041454d25185f8eea7e34916bf4177cb62
168742464+Sachin-A-Purkar@users.noreply.github.com <168742464+Sachin-A-Purkar@users.noreply.github.com>
2025-03-15 15:48:52 +0530

    update

--- web_project/urls.py
+++ web_project/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('Calculator/',include("Calculator.urls")),
+    path('', include('Calculator.urls')),
     path('admin/', admin.site.urls)
 ]
