e34cebf811e17decb98b2a4ce6af1272ad86f4b4
168742464+Sachin-A-Purkar@users.noreply.github.com <168742464+Sachin-A-Purkar@users.noreply.github.com>
2025-07-23 13:59:38 +0530

    up

--- web_project/settings.py
+++ web_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)t3!g*gua4_^fs0cuufu0%%51sf@rc9w@3v_6b##kry*wp!q6=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
